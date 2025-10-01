9e6259625d78d3b36a5551c7a22c3c1199cf2303
Shobhit Jain <shobhitj1805@gmail.com>
2025-10-02 00:19:28 +0530

    DB connected

--- src/app.js
+++ src/app.js
@@ -1,23 +1,16 @@
 const express = require('express');
-
+const connectDB = require('./config/database');
 const app = express();
 
-const {adminAuth, userAuth} = require('./middlewares/auth');
-
-app.use("/admin", adminAuth);
-
-app.get("/user", userAuth, (req, res) => {
-  res.send("User data sent");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a user");
-});
-
-app.listen(7777, () => {
+connectDB()
+.then(() => {
+    console.log("Database connected successfully");
+   app.listen(7777, () => {
   console.log('Server is successfully listening on port 7777');
 });
+})
+.catch((err) => {
+    console.error("Database connection failed");
+});
+
+
