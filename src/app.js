c5e1aace99d178936db13a910f7b5f4290578c48
Shobhit Jain <shobhitj1805@gmail.com>
2025-10-01 16:54:20 +0530

    Initial API's done

--- src/app.js
+++ src/app.js
@@ -2,11 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hey", (req, res) => {
-  res.send('Hello World!');
+app.get("/user", (req, res) => {
+  res.send({firstName: "Shobhit", lastName: "Jain"});
 });
-app.use("/", (req, res) => {
-  res.send('Hello from the dashboard!');
+
+app.post("/user", (req, res) => {
+  //saving data to database
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+
+app.use("/test", (req, res) => {
+  res.send('Hello from the server!');
 });
 
 app.listen(7777, () => {
